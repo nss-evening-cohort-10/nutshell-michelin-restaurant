c44ed57daba894ba8bc17722b1fd778b5cddd86b
Gabriel Seals <sealsgj@gmail.com>
2019-11-16 14:56:11 -0600

    WIP

---
+++ src/javascripts/components/Employees/employees.js
@@ -0,0 +1,1 @@
+import './employees.scss';
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,12 +2,16 @@ import 'bootstrap';
 import '../styles/main.scss';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import $ from 'jquery';
 import apiKeys from './helpers/apiKeys.json';
 import reservations from './components/Reservations/reservations';
+import printHome from './components/Home/home';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseConfig);
   reservations.printReservations();
+  printHome.printHome();
+  $('body').on('click', '.activateHome', printHome.printHome());
 };
 
 init();
