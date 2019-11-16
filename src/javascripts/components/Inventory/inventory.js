4d4eeb082b364afdad468e421d752ded9e5e6f2f
Crystal Broach <crysland@gmail.com>
2019-11-16 13:41:43 -0600

    reading data from firebase

---
+++ src/javascripts/components/Inventory/inventory.js
@@ -0,0 +1,27 @@
+import inventoryData from '../../helpers/data/inventoryData';
+import utilities from '../../helpers/utilities';
+
+import './inventory.scss';
+
+const printIngredients = () => {
+  inventoryData.getInventory()
+    .then((ingredients) => {
+      console.log(ingredients);
+      let domString = `
+      <h2>Inventory</h2>
+      `;
+      ingredients.forEach((ingredient) => {
+        domString += `
+        <p>${ingredient.name}</p>
+        `;
+      });
+      utilities.printToDom('printComponent', domString);
+    })
+    .catch((error) => console.error(error));
+};
+
+const init = () => {
+  printIngredients();
+};
+
+export default { init };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,10 +4,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import apiKeys from './helpers/apiKeys.json';
 import reservations from './components/Reservations/reservations';
+import inventory from './components/Inventory/inventory';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseConfig);
   reservations.printReservations();
+  inventory.init();
 };
 
 init();
