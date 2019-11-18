c891b03cf5443ff466080798f0cbe1aee6b3a71c
Crystal Broach <crysland@gmail.com>
2019-11-17 18:05:30 -0600

    add working, modal still needs work

--- src/javascripts/components/Inventory/inventory.js
+++ src/javascripts/components/Inventory/inventory.js
@@ -1,14 +1,33 @@
+import $ from 'jquery';
 import inventoryData from '../../helpers/data/inventoryData';
 import makeIngredientCard from '../MakeIngredientCard/makeIngredientCard';
 import utilities from '../../helpers/utilities';
 
 import './inventory.scss';
 
+const createNewIngredient = (e) => {
+  e.stopImmediatePropagation();
+  const newIngredient = {
+    ingredientName: $('#ingredient-name').val(),
+    amountStocked: $('#amount-stocked').val(),
+    uom: $('#unit-of-measurement').val(),
+    cost: $('#ingredient-cost').val(),
+  };
+  inventoryData.addIngredient(newIngredient)
+    .then(() => {
+      $('#addIngredientModal').modal('hide');
+      // eslint-disable-next-line no-use-before-define
+      printIngredients();
+    })
+    .catch((error) => console.error(error));
+};
+
 const printIngredients = () => {
   inventoryData.getInventory()
     .then((ingredients) => {
       let domString = `
       <h2>Inventory</h2>
+      <button class="btn btn-secondary cudButton" id="addNewIngredient">Add Ingredient</button>
       <div class="container mx-auto">
       <div class="d-flex flex-wrap flex-row">
       `;
@@ -17,6 +36,7 @@ const printIngredients = () => {
       });
       domString += '</div></div>';
       utilities.printToDom('printComponent', domString);
+      $('#addNewIngredient').on('click', createNewIngredient);
     })
     .catch((error) => console.error(error));
 };
