8189faebf5c60306e43fb0cca11fea4af8d5addd
Ashley Claiborne <languageisaliquid@gmail.com>
2019-11-19 00:18:13 -0600

    menu delete complete

--- src/javascripts/helpers/data/MenuIngredientData.js
+++ src/javascripts/helpers/data/MenuIngredientData.js
@@ -3,17 +3,21 @@ import apiKey from '../apiKeys.json';
 
 const baseUrl = apiKey.firebaseConfig.databaseURL;
 
+const deleteMenuIngredients = (menuIngredientId) => axios.delete(`${baseUrl}/menuItemIngredients/${menuIngredientId}.json`);
+
 const getAllMenuIngredients = () => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/menuItemIngredients.json`)
     .then((response) => {
       const menuIngredients = response.data;
-      const menuIngredientsArr = [];
-      Object.keys(menuIngredients).forEach((fbId) => {
-        menuIngredients[fbId].id = fbId;
-        menuIngredientsArr.push(menuIngredients[fbId]);
-      });
-      resolve(menuIngredientsArr);
+      if (menuIngredients) {
+        const menuIngredientsArr = [];
+        Object.keys(menuIngredients).forEach((fbId) => {
+          menuIngredients[fbId].id = fbId;
+          menuIngredientsArr.push(menuIngredients[fbId]);
+        });
+        resolve(menuIngredientsArr);
+      }
     }).catch((err) => reject(err));
 });
 
-export default { getAllMenuIngredients };
+export default { getAllMenuIngredients, deleteMenuIngredients };
--- src/javascripts/helpers/data/menuData.js
+++ src/javascripts/helpers/data/menuData.js
@@ -1,19 +1,33 @@
 import axios from 'axios';
 import apiKey from '../apiKeys.json';
+import utilities from '../utilities';
 
 const baseUrl = apiKey.firebaseConfig.databaseURL;
+
+const deleteMenuItem = (menuItemId) => axios.delete(`${baseUrl}/menuItems/${menuItemId}.json`);
 
 const getAllMenuItems = () => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/menuItems.json`)
     .then((response) => {
       const menuItems = response.data;
-      const menuItemsArr = [];
-      Object.keys(menuItems).forEach((fbId) => {
-        menuItems[fbId].id = fbId;
-        menuItemsArr.push(menuItems[fbId]);
-      });
-      resolve(menuItemsArr);
+      if (menuItems === null) {
+        const menuString = `
+          <h2 class="whiteh1">Menu</h2>
+          <button class="cudButton hide btn btn-secondary m-1"><i class="fas fa-plus cudButton hide whiteh1 cursor">Add Menu Item</i></button>
+          <div class="container mx-auto">
+          <div class="d-flex flex-wrap flex-row">
+          <h1>It looks like you don't have anything on the menu. Consider adding something!</h1>
+        `;
+        utilities.printToDom('printComponent', menuString);
+      } else {
+        const menuItemsArr = [];
+        Object.keys(menuItems).forEach((fbId) => {
+          menuItems[fbId].id = fbId;
+          menuItemsArr.push(menuItems[fbId]);
+        });
+        resolve(menuItemsArr);
+      }
     }).catch((err) => reject(err));
 });
 
-export default { getAllMenuItems };
+export default { getAllMenuItems, deleteMenuItem };
