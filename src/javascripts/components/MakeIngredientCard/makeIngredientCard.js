ce9769991a8ff32cdcb6451480fac8afa53d1f78
Crystal Broach <crysland@gmail.com>
2019-11-19 23:41:53 -0600

    WIP

--- src/javascripts/components/MakeIngredientCard/makeIngredientCard.js
+++ src/javascripts/components/MakeIngredientCard/makeIngredientCard.js
@@ -6,7 +6,7 @@ const makeIngredientCard = (ingredient) => {
   const domString = `
   <div class="card ingredient-card col-3 pt-0 pr-1 pl-1" id="${ingredient.id}-card">
     <div class="card-body text-center pt-2 pb-2">
-      <h3 class="card-title text-left">${ingredient.name}</h3>
+      <h3 class="card-title text-left ingredient-titles">${ingredient.name}</h3>
       <p class="text-left">Cost: ${costFormatted}</p>
       <p class="text-left">Amount Stocked: ${ingredient.amountStocked} ${ingredient.unitOfMeasurement}</p>
       <div class="card-footer ingredientCardFooter">
