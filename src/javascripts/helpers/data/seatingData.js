689e2d11838052cc5cebeaa3d3105c4a5b994d8c
Jacob <wittenberg.jacob@gmail.com>
2019-11-30 13:12:28 -0600

    committing to save notes and check kelsey's pr

--- src/javascripts/helpers/data/seatingData.js
+++ src/javascripts/helpers/data/seatingData.js
@@ -3,6 +3,7 @@ import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseConfig.databaseURL;
 
+// console log the shit out of this first thing. each line. figure it out. seems more complex than necessary?
 const getSeating = () => new Promise((resolve, reject) => {
   axios.get(`${baseUrl}/seatings.json`)
     .then((response) => {
@@ -19,3 +20,17 @@ const getSeating = () => new Promise((resolve, reject) => {
 });
 
 export default { getSeating };
+
+// Function to:
+// pull tableName from db/seatings.json into seatingsData.js
+// convert from json into array, export array
+
+// Function to:
+// - change input field to dropdown menu
+// - generate dropdown selection fields based on table numbers array from seatingsData.js
+
+// *then*
+// - function to collect dropdown selection
+// and apply it to an axios POST call to add the table number to the reservation data within the database.
+
+// - *then in a separate ticket* make so dynamically only prints tables to dropdown that are not currently assigned to a reservation
